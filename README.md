2aa737ad1393151ecbd636f41e4f71bc90f9ab6f
Dan Kantor <dan@ex.fm>
2012-09-07 11:22:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# iex-audio
+
+
+
+
+## Install
+
+
+     npm install iex-audio
+
+## Testing
+
+    git clone 
+    npm install
+    open test/index.html
---
+++ test/iex-audio.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("iex-audio", function(){
+    it("should have at least one test", function(){
+        assert.fail("Write a test!");
+    });
+});
